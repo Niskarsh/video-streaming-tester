bf3ea39291fe8a9726c3c7c2109b459c69270133
Milind Mishra <milind.mishra4@gmail.com>
2024-08-24 00:05:37 +0530

    📦 NEW: add t3 env and update secrets from env

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,120 @@
-'use client';
-import { useState, useRef } from 'react';
-import { Upload } from "@aws-sdk/lib-storage";
-import { S3Client } from "@aws-sdk/client-s3";
-import { v4 as uuidv4 } from 'uuid';
+'use client'
+
+import { useState, useRef } from 'react'
+import { Upload } from '@aws-sdk/lib-storage'
+import { S3Client } from '@aws-sdk/client-s3'
+import { v4 as uuidv4 } from 'uuid'
+import { env } from '@/env'
 
 export default function LiveScreenStreaming() {
-  const [isStreaming, setIsStreaming] = useState(false);
-  const screenVideoRef = useRef<HTMLVideoElement>(null);
-  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
-  const streamOpenRef = useRef<boolean>(true);
+  const [isStreaming, setIsStreaming] = useState(false)
+  const screenVideoRef = useRef<HTMLVideoElement>(null)
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null)
+  const streamOpenRef = useRef<boolean>(true)
   const s3 = new S3Client({
-    region: '********',
+    region: env.NEXT_PUBLIC_AWS_REGION,
     credentials: {
-      accessKeyId: '********',
-      secretAccessKey: '*********',
+      accessKeyId: env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+      secretAccessKey: env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
     },
     useAccelerateEndpoint: true,
-  });
+  })
 
   const startStreaming = async () => {
-    const mediaStream = await navigator.mediaDevices.getDisplayMedia({ video: true });
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+    })
     if (screenVideoRef.current) {
-      screenVideoRef.current.srcObject = mediaStream;
+      screenVideoRef.current.srcObject = mediaStream
     }
 
-    const mediaRecorder = new MediaRecorder(mediaStream, { mimeType: 'video/webm; codecs=vp9' });
-    mediaRecorderRef.current = mediaRecorder;
+    const mediaRecorder = new MediaRecorder(mediaStream, {
+      mimeType: 'video/webm; codecs=vp9',
+    })
+    mediaRecorderRef.current = mediaRecorder
 
     const readableStream = new ReadableStream({
       start(controller) {
-      
         mediaRecorder.ondataavailable = async (event) => {
           if (event.data.size > 0 && streamOpenRef.current) {
-            let data = event.data;
-            const chunkSize = 64 * 1024;  // 64 KB chunks
-            for (let i = 0; i < data.size; i += chunkSize) {
-              const chunk = data.slice(i, i + chunkSize);
-              let buffer = Buffer.from(new Uint8Array(await chunk.arrayBuffer()));
-              controller.enqueue(buffer);
+            const chunkSize = 64 * 1024 // 64 KB chunks
+            for (let i = 0; i < event.data.size; i += chunkSize) {
+              const chunk = event.data.slice(i, i + chunkSize)
+              const buffer = Buffer.from(
+                new Uint8Array(await chunk.arrayBuffer())
+              )
+              controller.enqueue(buffer)
             }
           }
-        };
+        }
+
         mediaRecorder.onstop = () => {
-          streamOpenRef.current = false;
-          controller.close(); // Close the stream when recording stops
-        };
-      }
-    });
-    const bucketName = 'posk-content-dev';
-    const key = `live-stream-${uuidv4()}.webm`;
+          streamOpenRef.current = false
+          controller.close() // Close the stream when recording stops
+        }
+      },
+    })
+
+    const key = `live-stream-${uuidv4()}.webm`
 
     const upload = new Upload({
       client: s3,
-      params: { Bucket: bucketName, Key: key, Body: readableStream },
-
-      // optional tags
+      params: {
+        Bucket: env.NEXT_PUBLIC_AWS_BUCKET_NAME,
+        Key: key,
+        Body: readableStream,
+      },
       tags: [
-        /*...*/
+        // optional tags
       ],
-
-      // additional optional fields show default values below:
-
       // (optional) concurrency configuration
       queueSize: 8,
-
       // (optional) size of each part, in bytes, at least 5MB
       partSize: 1024 * 1024 * 5,
-
       // (optional) when true, do not automatically call AbortMultipartUpload when
       // a multipart upload fails to complete. You should then manually handle
       // the leftover parts.
       leavePartsOnError: false,
-    });
+    })
 
-    upload.on("httpUploadProgress", (progress) => {
-      console.log(`Uploaded part: ${progress.loaded}/${progress.total}`, progress);
-    });
+    upload.on('httpUploadProgress', (progress) => {
+      console.log(
+        `Uploaded part: ${progress.loaded}${
+          progress.total ? `/${progress.total}` : ''
+        }`,
+        progress
+      )
+    })
 
-
-
-
-
-    mediaRecorder.start(100); // Capture in small chunks for low latency
-    setIsStreaming(true);
+    mediaRecorder.start(100) // Capture in small chunks for low latency
+    setIsStreaming(true)
     try {
-      await upload.done();
-      console.log('Upload completed');
+      await upload.done()
+      console.log('Upload completed')
     } catch (err) {
-      console.error('Error uploading to S3:', err);
+      console.error('Error uploading to S3:', err)
     }
-  };
+  }
 
   const stopStreaming = () => {
     if (mediaRecorderRef.current) {
-      mediaRecorderRef.current.stop();
+      mediaRecorderRef.current.stop()
     }
-    setIsStreaming(false);
-  };
+    setIsStreaming(false)
+  }
 
   return (
     <div>
       <h1>Live Screen Streaming</h1>
       <video id="screenVideo" ref={screenVideoRef} autoPlay muted></video>
-      <button id="startButton" className='bg-red-300 p-4 mr-4' onClick={startStreaming} disabled={isStreaming}>
-        Start Streaming
-      </button>
-      <button id="stopButton" className='bg-red-300 p-4' onClick={stopStreaming} disabled={!isStreaming}>
-        Stop Streaming
+      <button
+        type="button"
+        className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        onClick={isStreaming ? stopStreaming : startStreaming}
+        disabled={isStreaming}
+      >
+        {isStreaming ? 'Stop Streaming' : 'Start Streaming'}
       </button>
     </div>
-  );
+  )
 }
